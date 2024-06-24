bb9ab022b1402f6ef9688eb5fc52266f043dc80d
吃掉大象的蚂蚁 <eoe2005@qq.com>
2024-06-24 22:30:51 +0800

    fx

--- godevenv/action/init.go
+++ godevenv/action/init.go
@@ -2,12 +2,14 @@ package action
 
 import (
 	"fmt"
+	"os"
 	"runtime"
 
 	"github.com/ghf-go/goapp/base"
 )
 
 func Run() {
+	os.Setenv("ghf", "123")
 	fmt.Println(runtime.GOOS, runtime.GOARCH, runtime.GOROOT())
 	base.RunAction(0, map[string]base.ActionFunc{
 		"help":  helpAction,
