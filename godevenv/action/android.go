65703eb30abbe97ebb06b4d48fb94252fbc5245f
吃掉大象的蚂蚁 <eoe2005@qq.com>
2024-06-28 17:37:55 +0800

    fx

--- godevenv/action/android.go
+++ godevenv/action/android.go
@@ -13,7 +13,7 @@ func androidAction() {
 	switch runtime.GOOS {
 	case "darwin":
 		// base.ShRun("wget https://googledownloads.cn/android/repository/commandlinetools-mac-11076708_latest.zip ; unzip commandlinetools-mac-11076708_latest.zip;mkdir -p ~/sdk;mv cmdline-tools ~/sdk/;rm -rf commandlinetools-mac-11076708_latest.zip;echo 'PATH=$PATH:~/sdk/cmdline-tools/bin' >> ~/.bashrc;echo 'PATH=$PATH:~/sdk/cmdline-tools/bin' >> ~/.zshrc")
-		base.ShRun("brew install java ;brew casks install android-commandlinetools  android-sdk android-messages android-ndk  android-platform-tools  androidtool  android-studio flutter")
+		base.ShRun("brew install java gradle ;brew casks install android-commandlinetools  android-sdk android-messages android-ndk  android-platform-tools  androidtool  android-studio flutter")
 	case "linux":
 	case "win":
 	}
